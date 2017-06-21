3cbfea6566a2e91fc3732231fa261ac13b14788e
Ryan Kramer <RKramer613@gmail.com>
2017-06-21 16:43:51 -0700

    Added scenarios

--- main.js
+++ main.js
@@ -8,3 +8,6 @@ var outcome;
 // Alert the player with the Introduction to the story and game.
 window.alert("The world has come to an end, and is now infested with the walking dead. There is no hope, no cure, only survival. Will you survive?");
 
+// Provide the player with a random beginning scerario. Create an array of several options.
+var beginningScenarios = ["You wake up in the hospital. It is eerily quiet. You tiptoe to the door and peek into the hall...", "You are sitting in the living room of your boarded up house. Your father and brother have gone out scavenging, but have yet to return...", "You are running down a long road surrounded on each side by trees. There is a horde of zombies chasing you. Suddenly, you trip..."];
+
