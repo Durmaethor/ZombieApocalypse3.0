bda2d8024fc990d49c3c4ff213ebf4d6922c582b
Ryan Kramer <RKramer613@gmail.com>
2017-06-21 16:17:01 -0700

    Outcome and intro

---
+++ main.js
@@ -0,0 +1,10 @@
+// This is a simple random adventure game created in JavaScript. 
+
+// ZOMBIE APOCALYPSE 3.0
+
+// Declare a variable to store the outcome of the game.
+var outcome;
+
+// Alert the player with the Introduction to the story and game.
+window.alert("The world has come to an end, and is now infested with the walking dead. There is no hope, no cure, only survival. Will you survive?");
+
