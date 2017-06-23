9412a328cbda929615813fa1ca66a28c1429277a
Ryan Kramer <RKramer613@gmail.com>
2017-06-23 11:32:31 -0700

    Added class outcome to first switch case. Testing

--- main.js
+++ main.js
@@ -107,12 +107,37 @@ switch(choice) {
     case 'WEAPON':
         var weapon = prompt("You look around the room and see a BONESAW, a NEEDLE, or a DEFIBRILLATOR").toUpperCase();
         if(weapon === 'BONESAW') {
-            window.alert("You grab the bonesaw and move towards the door. Suddenly a zombie bursts through the door and rushes you. You move to swing the bonesaw, but your arm stops mid-swing. You realize in horror that the bonesaw is plugged in, but your chance is gone and the zombie is already upon you. You feel a sharp pain in your shoulder and realize that you have been bitten. You feel warm liquid running freely down your back and know that this is the end...");
-            
-            // Set outcome variable to LOSE
-            outcome = "die";
+            if(character.characterClass === 'SOLDIER') {
+                window.alert("You grab the bonesaw and move towards the door. Suddenly a zombie bursts through the door and rushes you. You move to swing the bonesaw, but your arm stops mid-swing. You realize in horror that the bonesaw is plugged in, but fortunately in the previous world you were a " + character.characterClass + " and your strength is unmatched. You continue the swing, pulling the cord from the wall and hitting the zombie square in the forehead." );
+
+                // Set outcome to "survive"
+                outcome = 'survive';
+            } 
+            else {
+                window.alert("You grab the bonesaw and move towards the door. Suddenly a zombie bursts through the door and rushes you. You move to swing the bonesaw, but your arm stops mid-swing. You realize in horror that the bonesaw is plugged in, but your chance is gone and the zombie is already upon you. You feel a sharp pain in your shoulder and realize that you have been bitten. You feel warm liquid running freely down your back and know that this is the end...");
+                
+                // Set outcome variable to DIE
+                outcome = "die";
         }
-        break;
+        else if(weapon === 'NEEDLE') {
+            var stab = window.prompt("You grab the needle and position yourself behind the door. A zombie bursts into the room and walks right past you. You slowly move up behind the zombie and grab it from behind. Where do you stab the zombie? In the NECK, HEART, or EYE?").toUpperCase();
+            if(stab === 'NECK') {
+                window.alert("You are a fool and stab the zombie in the neck, lodging the needle into its spine where it gets stuck. The zombie shakes you off and turns on you...");
+                
+                //set outcome variable to DIE
+                outcome = "die";
+            }
+            else if(stab === 'HEART') {
+                window.alert("Your extensive knowledge of anatomy leads you to plunge the needle into the zombie's heart. You know for sure this will kill the zombie. Unfortunately you are wrong. You scream in horror as you realize too late that this will not kill a zombie...")
+
+                // set outcome to DIE
+                outcome = "die";
+            }
+            else if(stab === 'EYE') {
+                window.alert("")
+            }
+        }
+        break; 
 }
 
 // Check to see if an outcome has been assigned and alert appropriate result
